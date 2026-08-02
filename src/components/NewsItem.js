be30609836ab86ddb48199bc21d2f2200800d4d2
Ayush <poudelh002@gmail.com>
2026-08-02 09:34:08 +0545

    Adjust border radius in NewsItem component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -19,7 +19,7 @@ import React from 'react'
           >
             <span
               className="p-2 border border-light"
-              style={{ backgroundColor: 'lightgreen', borderRadius: '10px'}}
+              style={{ backgroundColor: 'lightgreen', borderRadius: '7px'}}
             >
               {source}
             </span>
